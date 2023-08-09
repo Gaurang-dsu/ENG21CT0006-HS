ac49f0566ecb6f5fc0ece127dcbfe15aa1f4bbaf
Gaurang-dsu <141434924+Gaurang-dsu@users.noreply.github.com>
2023-08-09 20:35:34 +0530

    Create day3_3.hs

---
+++ day3_3.hs
@@ -0,0 +1,19 @@
+
+data Vehicle = Car String Int | Bike String Int deriving Show
+
+
+findVehicle :: Int -> [Vehicle] -> Maybe (String, Int)
+findVehicle index vehicles
+    | index < 0 || index >= length vehicles = Nothing  
+    | otherwise = case vehicles !! index of
+                    Car make year -> Just (make, year)
+                    Bike make year -> Just (make, year)
+
+main :: IO ()
+main = do
+    let vehicles = [Car "Toyota" 2010, Bike "Honda" 2022, Car "Ford" 2015]
+        index = 1
+        result = findVehicle index vehicles
+    case result of
+        Just (make, year) -> putStrLn $ "Vehicle at index " ++ show index ++ ": Make = " ++ make ++ ", Year = " ++ show year
+        Nothing -> putStrLn $ "Index " ++ show index ++ " is out of bounds."
